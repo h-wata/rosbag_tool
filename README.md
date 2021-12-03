ae4628754dabefb22077e92864b32fa9eeea355e
h-wata <hirofumi01.watanabe@g.softbank.co.jp>
2021-12-03 15:52:31 +0900

    add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+## command
+```
+python ./scripts/twist_heatmap.py input_bagfile.ba output_img.png
+```
--- scripts/twist_heatmap.py
+++ scripts/twist_heatmap.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
+# Copyright (c) 2021, h-wata.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import numpy as np
 import rosbag
 import os
@@ -11,7 +25,6 @@ args = sys.argv
 print(args)
 filename = os.path.normpath(os.path.join(os.getcwd(), args[1]))
 print(filename)
-# init
 
 # rosbag open
 bag = rosbag.Bag(filename)
@@ -26,7 +39,7 @@ for topic, msg, t in bag.read_messages():
     if topic == "/amcl_pose":
         np_pose_twist[0, 0] = msg.pose.pose.position.x
         np_pose_twist[0, 1] = msg.pose.pose.position.y
-    # robot_poseの方が寄さそうだけど、取り忘れた
+    # robot_poseの方が寄さそうだけど、bag取り忘れた
     # if topic == "/robot_pose":
     #     np_pose_twist[0, 0] = msg.pose.position.x
     #     np_pose_twist[0, 1] = msg.pose.position.y
@@ -36,9 +49,9 @@ for topic, msg, t in bag.read_messages():
         else:
             dataset = np.append(dataset, np_pose_twist, axis=0)
 
-# csv形式で保存
+# save as csv.
 np.savetxt('./text.csv', dataset, delimiter=',', fmt='%.5f')
-# 散布図の描画
+# plot scatter 
 plt.figure()
 plt.scatter(dataset[:, 0], dataset[:, 1], s=5, c=dataset[:, 3], cmap='jet')
 plt.colorbar()
